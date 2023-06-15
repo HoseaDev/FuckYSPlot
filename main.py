6569c7414a85f53fbec5ff7085060142ce6ec1be
Hosea <hoseadev@gmail.com>
2023-06-15 22:22:48 +0800

    升级1.0.1版本。1

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ class FuckerUI(ttk.Frame):
         self.pack(fill=BOTH, expand=YES)
 
 
+
         self.runStateStr = ttk.StringVar(value="")
         self.isFuckPlot = ttk.BooleanVar(value=True)
         self.isPickUp = ttk.BooleanVar(value=False)
